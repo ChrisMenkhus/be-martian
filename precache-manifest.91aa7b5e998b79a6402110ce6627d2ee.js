8e41a5665e476c9a5fd7a30ac3a99c37c0b64320
Chris Menkhus <menkhus.chris@gmail.com>
2020-07-23 11:41:09 -0600

    Updates

---
+++ precache-manifest.91aa7b5e998b79a6402110ce6627d2ee.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "623a7dff3812dcc3e7497103749423f9",
+    "url": "/be-martian/index.html"
+  },
+  {
+    "revision": "fc4afec46a90d7d62fc0",
+    "url": "/be-martian/static/css/main.b4d3a40f.chunk.css"
+  },
+  {
+    "revision": "6f625c5eedf6f38f52fd",
+    "url": "/be-martian/static/js/2.78566745.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/be-martian/static/js/2.78566745.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc4afec46a90d7d62fc0",
+    "url": "/be-martian/static/js/main.22fe41a6.chunk.js"
+  },
+  {
+    "revision": "7cc97fc755c1f1ee3c5a",
+    "url": "/be-martian/static/js/runtime-main.0c61003c.js"
+  },
+  {
+    "revision": "a10e779cb534b7bef39aec27e871c920",
+    "url": "/be-martian/static/media/orbits.a10e779c.svg"
+  },
+  {
+    "revision": "89144f946b30232aff520fc70c40877c",
+    "url": "/be-martian/static/media/planetandtree.89144f94.svg"
+  },
+  {
+    "revision": "cb6ac538207d89ef7e18f5765c08ceb8",
+    "url": "/be-martian/static/media/rocket.cb6ac538.svg"
+  },
+  {
+    "revision": "1b9714f6711087bdb4b84e6c31dc9e80",
+    "url": "/be-martian/static/media/stars.1b9714f6.jpg"
+  },
+  {
+    "revision": "cd8b33f80952e084ae5d3c944feadd0c",
+    "url": "/be-martian/static/media/stars2.cd8b33f8.jpg"
+  },
+  {
+    "revision": "805ef50db0fd98ea53bc281d654d68a4",
+    "url": "/be-martian/static/media/stars3.805ef50d.jpg"
+  }
+]);
